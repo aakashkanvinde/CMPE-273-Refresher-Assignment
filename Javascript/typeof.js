79fb650535690868d16dbe96ee69ff5c3e62e288
Aakash R Kanvinde <kanvindeakash@gmail.com>
2019-02-14 22:06:03 -0800

    Add files via upload

---
+++ Javascript/typeof.js
@@ -0,0 +1,20 @@
+var statement = "Hello this is a statement";
+var age = 10;
+var decision = true;
+var name;
+var gender = null;
+var person = function person(){
+    console.log("Hello I am "+name);
+}
+
+console.log("\n***** Javascript - typeOf *****\n");
+console.log("\n typeof statement: "+typeof(statement)+"\n");
+console.log("\n typeof age: "+typeof(age)+"\n");
+
+console.log("\n typeof decision: "+typeof(decision)+"\n");
+
+console.log("\n typeof name: "+typeof(name)+"\n");
+
+console.log("\n typeof gender: "+typeof(gender)+"\n");
+
+console.log("\n typeof person: "+typeof(person)+"\n");
