e51da5dc30a7514d8d8d88a5fe41b4e8d82c67fc
Aakash R Kanvinde <kanvindeakash@gmail.com>
2019-02-15 20:19:54 -0800

    Add files via upload

---
+++ Javascript/strict.js
@@ -0,0 +1,9 @@
+function strictMode(){
+    //using strict will enable the browser to NOT use undeclared variables
+    "use strict"; 
+
+    name = "Aakash";
+    console.log(name);
+}
+
+strictMode();
